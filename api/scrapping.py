4f9035a1b1609f678fd895243794981ce73a729b
rubsuadav <rsuarezdavid@gmail.com>
2024-03-01 01:21:10 +0100

    refactoring and improving the dinamic scrapping

--- api/scrapping.py
+++ api/scrapping.py
@@ -34,7 +34,7 @@ def validate_params(url, tags):
     check_url = is_valid_url(url)
     if isinstance(check_url, HttpResponse):
         return check_url
-    elif not (isinstance(tags, list) or isinstance(tags, str)):
+    elif not (isinstance(tags, list)):
         return HttpResponse("Error: El atributo 'tags' no es válido")
 
 
@@ -47,16 +47,16 @@ def scrapping():
     driver.get(url)
 
     # Obtener todo el texto de la página
-    root = driver.find_element(By.TAG_NAME, "body").tag_name
-    elements = driver.find_elements(By.TAG_NAME, root)
+    body = driver.find_element(By.TAG_NAME, "body")
+
+    # Excluir los elementos dentro de 'header' y 'footer'
+    elements = body.find_elements(
+        By.XPATH, f".//*[not(ancestor::header) and not(ancestor::footer)]")
+
     for element in elements:
-        tag_name = element.tag_name
-        if tag_name != 'header' and tag_name != "footer":
-            for tag in tags:
-                print(tag)
-                try:
-                    main_element = element.find_element(By.TAG_NAME, tag)
-                    return HttpResponse(main_element.text)
-                except:
-                    continue
+        for tag in tags:
+            print(element.tag_name)
+            if element.tag_name == tag:
+                return HttpResponse(element.text)
+
     driver.quit()
